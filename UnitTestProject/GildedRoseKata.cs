e4c518a035af66711c5eb04ad423086164718158
adelinelim <adeline_yiing@hotmail.com>
2014-12-22 17:50:46 +0800

    csharp gilded rose kata

---
+++ UnitTestProject/GildedRoseKata.cs
@@ -0,0 +1,233 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject
+{
+    public class ShoppingCart
+    {
+        private readonly ICalculationRules _calculationRules;
+        public ShoppingCart()
+        {
+            _calculationRules = new CalculationRules();
+            Items = new List<Item>();
+        }
+
+        public List<Item> Items
+        {
+            get;
+            set;
+        }
+
+        public string Run()
+        {
+            string result = "";
+
+            foreach (var i in Items)
+            {
+                _calculationRules.UpdateItem(i);
+                result = result + "\r\n" + "Name: " + i.Name + "\tSellIn: " + i.SellIn + "\tQuanlity: " + i.Quality;
+            }
+            result += "\r\n";
+            return result;
+        }
+    }
+
+    public static class CalculationHelper
+    {
+        public static readonly int MAX_QUALITY = 50;
+        public static readonly int MIN_QUALITY = 0;
+
+        public static void UpdateSellIn(Item item)
+        {
+            if (!IsLegendaryItem(item))
+                item.SellIn -= 1;
+        }
+
+        public static bool IsLegendaryItem(Item item)
+        {
+            return item.Name.Equals("Sulfuras, Hand of Ragnaros");
+        }
+
+        public static bool IsExpired(Item item)
+        {
+            return item.SellIn <= 0;
+        }
+    }
+
+    public interface ICalculationRules
+    {
+        void UpdateItem(Item item);
+    }
+
+    public class CalculationRules : ICalculationRules
+    {
+        private readonly List<ICalculateQualityStrategy> _qualityStrategies;
+
+
+        public CalculationRules()
+        {
+            _qualityStrategies = new List<ICalculateQualityStrategy>();
+            _qualityStrategies.Add(new CalculateAgedItemStrategy());
+            _qualityStrategies.Add(new CalculateConjuredItemStrategy());
+            _qualityStrategies.Add(new CalculateNormalItemStrategy());
+            _qualityStrategies.Add(new CalculateSpecialAgeingItemStrategy());
+            _qualityStrategies.Add(new CalculateLegendaryItemStrategy());
+        }
+
+        public void UpdateItem(Item item)
+        {
+            item.Quality = _qualityStrategies.First(r => r.IsMatch(item)).CalculateQuality(item);
+            CalculationHelper.UpdateSellIn(item);
+        }
+    }
+
+    public interface ICalculateQualityStrategy
+    {
+        bool IsMatch(Item item);
+        int CalculateQuality(Item item);
+    }
+
+    public class CalculateAgedItemStrategy : ICalculateQualityStrategy
+    {
+        public bool IsMatch(Item item)
+        {
+            return item.Name.Equals("Aged Brie");
+        }
+
+        public int CalculateQuality(Item item)
+        {
+            int quality = item.Quality;
+
+            if (CalculationHelper.IsExpired(item))
+            {
+                quality += 2;
+            }
+            else
+            {
+                quality += 1;
+            }
+
+            if (quality > CalculationHelper.MAX_QUALITY)
+                quality = CalculationHelper.MAX_QUALITY;
+
+            return quality;
+        }
+    }
+
+    public class CalculateConjuredItemStrategy : ICalculateQualityStrategy
+    {
+        public bool IsMatch(Item item)
+        {
+            return item.Name.Equals("Conjured Mana Cake");
+        }
+
+        public int CalculateQuality(Item item)
+        {
+            int quality = item.Quality;
+
+            if (CalculationHelper.IsExpired(item))
+            {
+                quality -= 4;
+            }
+            else
+            {
+                quality -= 2;
+            }
+
+            if (quality < CalculationHelper.MIN_QUALITY)
+                quality = CalculationHelper.MIN_QUALITY;
+
+            return quality;
+        }
+    }
+
+    public class CalculateNormalItemStrategy : ICalculateQualityStrategy
+    {
+        public bool IsMatch(Item item)
+        {
+            return
+                !CalculationHelper.IsLegendaryItem(item) && 
+                !item.Name.Equals("Conjured Mana Cake") && 
+                !item.Name.Equals("Aged Brie") && 
+                !item.Name.Equals("Backstage passes to a TAFKAL80ETC concert");
+        }
+
+        public int CalculateQuality(Item item)
+        {
+            int quality = item.Quality;
+
+            if (CalculationHelper.IsExpired(item))
+            {
+                quality -= 2;
+            }
+            else
+            {
+                quality -= 1;
+            }
+
+            if (quality < CalculationHelper.MIN_QUALITY)
+                quality = CalculationHelper.MIN_QUALITY;
+
+            return quality;
+        }
+    }
+
+    public class CalculateSpecialAgeingItemStrategy : ICalculateQualityStrategy
+    {
+        public bool IsMatch(Item item)
+        {
+            return item.Name.Equals("Backstage passes to a TAFKAL80ETC concert");
+        }
+
+        public int CalculateQuality(Item item)
+        {
+            int quality = item.Quality;
+
+            if (CalculationHelper.IsExpired(item))
+            {
+                quality = CalculationHelper.MIN_QUALITY;
+            }
+            else if (item.SellIn <= 5)
+            {
+                quality += 3;
+            }
+            else if (item.SellIn <= 10)
+            {
+                quality += 2;
+            }
+            else
+            {
+                quality += 1;
+            }
+
+            if (quality > CalculationHelper.MAX_QUALITY)
+                quality = CalculationHelper.MAX_QUALITY;
+
+            return quality;
+        }
+    }
+
+    public class CalculateLegendaryItemStrategy : ICalculateQualityStrategy
+    {
+        public bool IsMatch(Item item)
+        {
+            return CalculationHelper.IsLegendaryItem(item);
+        }
+
+        public int CalculateQuality(Item item)
+        {
+            return item.Quality;
+        }
+    }
+
+    //DO NOT MODIFY ITEM CLASS
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+    
+}
---
+++ UnitTestProject/UnitTestGildedRose.cs
@@ -0,0 +1,294 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ShoppingCartTest
+    {
+        private const string Aged = "Aged Brie";
+        private const string Elixir = "Elixir of the Mongoose";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Conjured = "Conjured Mana Cake";
+
+        [TestMethod]
+        public void LegendaryItem_BeforeSellDate_QualityAndSellIn_RemainUnchanged()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Sulfuras, 10, 80);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(80, cart.Items.First(x => x.Name == Sulfuras).Quality);
+            Assert.AreEqual(10, cart.Items.First(x => x.Name == Sulfuras).SellIn);
+        }
+
+        [TestMethod]
+        public void LegendaryItem_OnSellDate_QualityAndSellIn_RemainUnchanged()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Sulfuras, 0, 80);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(80, cart.Items.First(x => x.Name == Sulfuras).Quality);
+            Assert.AreEqual(0, cart.Items.First(x => x.Name == Sulfuras).SellIn);
+        }
+
+        [TestMethod]
+        public void LegendaryItem_AfterSellDate_QualityAndSellIn_RemainUnchanged()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Sulfuras, -1, 80);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(80, cart.Items.First(x => x.Name == Sulfuras).Quality);
+            Assert.AreEqual(-1, cart.Items.First(x => x.Name == Sulfuras).SellIn);
+        }
+
+        [TestMethod]
+        public void NormalItem_BeforeSellDate_Quality_ReducedByOne()
+        {
+            var cart = new ShoppingCart();
+            
+            var mockedItem = GetMockedItem(Elixir, 5, 7);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(6, cart.Items.First(x => x.Name == Elixir).Quality);
+            Assert.AreEqual(4, cart.Items.First(x => x.Name == Elixir).SellIn);
+        }
+
+        [TestMethod]
+        public void NormalItem_OnSellDate_Quality_ReduceByTwo()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Elixir, 0, 7);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(5, cart.Items.First(x => x.Name == Elixir).Quality);
+            Assert.AreEqual(-1, cart.Items.First(x => x.Name == Elixir).SellIn);
+        }
+
+        [TestMethod]
+        public void NormalItem_AfterSellDate_Quality_ReducedByTwo()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Elixir, -1, 7);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(5, cart.Items.First(x => x.Name == Elixir).Quality);
+            Assert.AreEqual(-2, cart.Items.First(x => x.Name == Elixir).SellIn);
+        }
+
+        [TestMethod]
+        public void NormalItem_WithZeroQuality_ReturnMinQuality()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Elixir, 2, 0);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(0, cart.Items.First(x => x.Name == Elixir).Quality);
+            Assert.AreEqual(1, cart.Items.First(x => x.Name == Elixir).SellIn);
+        }
+
+        [TestMethod]
+        public void AgedItem_BeforeSellDate_Quality_IncreasedByOne()
+        {
+            var cart = new ShoppingCart();
+
+            var mockedItem = GetMockedItem(Aged, 2, 0);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(1, cart.Items.First(x => x.Name == Aged).Quality);
+            Assert.AreEqual(1, cart.Items.First(x => x.Name == Aged).SellIn);
+        }
+
+        [TestMethod]
+        public void AgedItem_OnSellDate_Quality_IncreasedByTwo()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Aged, 0, 0);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(2, cart.Items.First(x => x.Name == Aged).Quality);
+            Assert.AreEqual(-1, cart.Items.First(x => x.Name == Aged).SellIn);
+        }
+
+        [TestMethod]
+        public void AgedItem_AfterSellDate_Quality_RemainUnchanged()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Aged, -1, 0);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(2, cart.Items.First(x => x.Name == Aged).Quality);
+            Assert.AreEqual(-2, cart.Items.First(x => x.Name == Aged).SellIn);
+        }
+
+        [TestMethod]
+        public void AgedItem_WithMaxQuality_ReturnMaxQuality()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Aged, 10, 50);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(50, cart.Items.First(x => x.Name == Aged).Quality);
+            Assert.AreEqual(9, cart.Items.First(x => x.Name == Aged).SellIn);
+        }
+
+        [TestMethod]
+        public void BackstageItem_On11SellDate_Quality_IncreasedByOne()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Backstage, 11, 20);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(21, cart.Items.First(x => x.Name == Backstage).Quality);
+            Assert.AreEqual(10, cart.Items.First(x => x.Name == Backstage).SellIn);
+        }
+
+        [TestMethod]
+        public void BackstageItem_On10SellDate_Quality_IncreasedByTwo()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Backstage, 10, 20);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(22, cart.Items.First(x => x.Name == Backstage).Quality);
+            Assert.AreEqual(9, cart.Items.First(x => x.Name == Backstage).SellIn);
+        }
+
+        [TestMethod]
+        public void BackstageItem_On6SellDate_Quality_IncreasedByTwo()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Backstage, 6, 20);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(22, cart.Items.First(x => x.Name == Backstage).Quality);
+            Assert.AreEqual(5, cart.Items.First(x => x.Name == Backstage).SellIn);
+        }
+
+        [TestMethod]
+        public void BackstageItem_On5SellDate_Quality_IncreasedByTwo()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Backstage, 5, 20);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(23, cart.Items.First(x => x.Name == Backstage).Quality);
+            Assert.AreEqual(4, cart.Items.First(x => x.Name == Backstage).SellIn);
+        }
+
+        [TestMethod]
+        public void BackstageItem_OnSellDate_Quality_DroppedToZero()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Backstage, 0, 20);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(0, cart.Items.First(x => x.Name == Backstage).Quality);
+            Assert.AreEqual(-1, cart.Items.First(x => x.Name == Backstage).SellIn);
+        }
+
+        [TestMethod]
+        public void BackstageItem_AfterSellDate_Quality_DroppedToZero()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Backstage, -1, 20);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(0, cart.Items.First(x => x.Name == Backstage).Quality);
+            Assert.AreEqual(-2, cart.Items.First(x => x.Name == Backstage).SellIn);
+        }
+
+        [TestMethod]
+        public void BackstageItem_WithMaxQuality_ReturnMaxQuality()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Backstage, 15, 50);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(50, cart.Items.First(x => x.Name == Backstage).Quality);
+            Assert.AreEqual(14, cart.Items.First(x => x.Name == Backstage).SellIn);
+        }
+
+        [TestMethod]
+        public void ConjuredItem_BeforeSellDate_Quality_IncreasedByOne()
+        {
+            var cart = new ShoppingCart();
+
+            var mockedItem = GetMockedItem(Conjured, 3, 6);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(4, cart.Items.First(x => x.Name == Conjured).Quality);
+            Assert.AreEqual(2, cart.Items.First(x => x.Name == Conjured).SellIn);
+        }
+
+        [TestMethod]
+        public void ConjuredItem_OnSellDate_Quality_IncreasedByTwo()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Conjured, 0, 6);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(2, cart.Items.First(x => x.Name == Conjured).Quality);
+            Assert.AreEqual(-1, cart.Items.First(x => x.Name == Conjured).SellIn);
+        }
+
+        [TestMethod]
+        public void ConjuredItem_AfterSellDate_Quality_RemainUnchanged()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Conjured, -1, 6);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(2, cart.Items.First(x => x.Name == Conjured).Quality);
+            Assert.AreEqual(-2, cart.Items.First(x => x.Name == Conjured).SellIn);
+        }
+
+        [TestMethod]
+        public void ConjuredItem_WithZeroQuality_ReturnMinQuality()
+        {
+            var cart = new ShoppingCart();
+            var mockedItem = GetMockedItem(Conjured, 3, 0);
+            cart.Items.Add(mockedItem);
+            cart.Run();
+
+            Assert.AreEqual(0, cart.Items.First(x => x.Name == Conjured).Quality);
+            Assert.AreEqual(2, cart.Items.First(x => x.Name == Conjured).SellIn);
+        }
+
+        private Item GetMockedItem(string itemName, int sellIn, int quality)
+        {
+            var items = MockRepository.GenerateStub<Item>();
+
+            items.Name = itemName;
+            items.Quality = quality;
+            items.SellIn = sellIn;
+
+            return items;
+        }
+    }
+}
